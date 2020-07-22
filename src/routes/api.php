02aa6760aa6519b4552c693ca29ab617f481eb61
Amir Hossein Jahani <me@iamir.net>
2020-07-22 22:05:42 +0430

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,15 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-
 Route::namespace('v1')->prefix('v1/windy')->middleware('auth:api')->group(function () {
     Route::apiResource('points', 'WindyPointController', ['as' => 'api.windy']);
-});
-Route::namespace('v1')->prefix('v1/windy/test')->group(function () {
-    Route::get('/', function () {
-        dd(\Carbon\Carbon::now()->subHours(3)->format('Y-m-d H:i:s'));
-        \iLaravel\iWindy\iApp\WindyPoint::resetRecordsId();
-        $data = \iLaravel\iWindy\Vendor\WindyPoint::handelImport();
-
-    });
 });
