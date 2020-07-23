32f3658a99256b7805b0d6ae983d0844d3ad3947
Amir Hossein Jahani <me@iamir.net>
2020-07-23 10:58:58 +0430

    Update WindyPoint.php

--- src/iapp/Http/Resources/WindyPoint.php
+++ src/iapp/Http/Resources/WindyPoint.php
@@ -9,7 +9,28 @@ class WindyPoint extends Resource
     public function toArray($request)
     {
         $data = parent::toArray($request);
-        $data = array_merge($data, WindyPointMeta::collection($this->meta)->groupBy('key')->toArray());
+        if (isset($request->level) && preg_match('/[0-9]/', $request->level)){
+            $first = $this->meta()
+                ->selectRaw("*, ABS(level -  {$request->level}) AS distance")
+                ->where('level', '>=', $request->level)
+                ->orderBy('level')
+                ->limit(6);
+            $meta = $this->meta()
+                ->selectRaw("*, ABS(level -  {$request->level}) AS distance")
+                ->where(function ($query) use ($request) {
+                    $query ->where('level', '>=', $request->level)
+                        ->orWhere('level', 'surface');
+                })
+                ->orderBy('level', 'desc')
+                ->limit(6)
+                ->union($first)
+                ->orderBy('distance')
+                ->get();
+        }elseif (isset($request->level) && preg_match('/[a-zA-Z]/', $request->level))
+            $meta = $this->meta->where('level', $request->level);
+        else
+            $meta = $this->meta;
+        $data = array_merge($data, WindyPointMeta::collection($meta)->groupBy('key')->toArray());
         unset($data['created_at']);
         unset($data['updated_at']);
         return $data;
